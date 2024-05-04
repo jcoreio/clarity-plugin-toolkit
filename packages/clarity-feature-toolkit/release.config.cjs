40be8dc80a7bd0dee394e04f3d7ed8c1334cd0b7
Andy Edwards <jedwards@fastmail.com>
2024-05-03 22:27:41 -0500

    chore: configure semantic-release to support package names in commit scopes

--- packages/clarity-feature-toolkit/release.config.cjs
+++ packages/clarity-feature-toolkit/release.config.cjs
@@ -1,4 +1,10 @@
 /* eslint-env node, es2018 */
+const path = require('path')
+const { name: pkg } = require('./package.json')
+const otherPackages = require('glob')
+  .sync('*/package.json', { cwd: path.dirname(__dirname) })
+  .map((f) => require(path.resolve(__dirname, '..', f)).name)
+
 module.exports = {
   branches: [
     '+([0-9])?(.{+([0-9]),x}).x',
@@ -8,47 +14,58 @@ module.exports = {
     { name: 'beta', prerelease: true },
     { name: 'alpha', prerelease: true },
   ],
-  tagFormat: '@jcoreio/clarity-feature-toolkit-v${version}',
+  tagFormat: `${pkg}-v\${version}`,
   plugins: [
     [
       require.resolve('@semantic-release/commit-analyzer'),
       {
-        preset: 'angular',
+        preset: 'conventionalcommits',
         releaseRules: [
-          {
-            breaking: true,
-            scope: '*@jcoreio/clarity-feature-toolkit*',
-            release: 'major',
-          },
-          {
-            revert: true,
-            scope: '*@jcoreio/clarity-feature-toolkit*',
-            release: 'patch',
-          },
-          {
-            type: 'feat',
-            scope: '*@jcoreio/clarity-feature-toolkit*',
-            release: 'minor',
-          },
-          {
-            type: 'fix',
-            scope: '*@jcoreio/clarity-feature-toolkit*',
-            release: 'patch',
-          },
-          {
-            type: 'perf',
-            scope: '*@jcoreio/clarity-feature-toolkit*',
-            release: 'patch',
-          },
+          ...[pkg, undefined].flatMap((scope) => [
+            { breaking: true, scope, release: 'major' },
+            { revert: true, scope, release: 'patch' },
+            { type: 'feat', scope, release: 'minor' },
+            { type: 'fix', scope, release: 'patch' },
+            { type: 'perf', scope, release: 'patch' },
+          ]),
           { scope: undefined, release: false },
         ],
       },
     ],
-    require.resolve('@semantic-release/release-notes-generator'),
+    [
+      require.resolve('@semantic-release/release-notes-generator'),
+      {
+        preset: 'conventionalcommits',
+        presetConfig: {
+          types: [
+            { type: 'build', section: 'Build System', hidden: true },
+            { type: 'chore', section: 'Build System', hidden: true },
+            { type: 'ci', section: 'Continuous Integration', hidden: true },
+            { type: 'style', section: 'Styles', hidden: true },
+            { type: 'test', section: 'Tests', hidden: true },
+            ...[
+              { type: 'docs', section: 'Documentation' },
+              { type: 'feat', section: 'Features' },
+              { type: 'fix', section: 'Bug Fixes' },
+              { type: 'perf', section: 'Performance Improvements' },
+              { type: 'refactor', section: 'Code Refactoring' },
+            ].flatMap((cfg) => [
+              { ...cfg, scope: pkg, hidden: false },
+              ...otherPackages.map((otherPkg) => ({
+                ...cfg,
+                scope: otherPkg,
+                hidden: true,
+              })),
+              { ...cfg, hidden: false },
+            ]),
+          ],
+        },
+      },
+    ],
     [
       require.resolve('@semantic-release/npm'),
       {
-        pkgRoot: require('path').join(__dirname, 'dist'),
+        pkgRoot: path.join(__dirname, 'dist'),
       },
     ],
     require.resolve('@semantic-release/github'),
