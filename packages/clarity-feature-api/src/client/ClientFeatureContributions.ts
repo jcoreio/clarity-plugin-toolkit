aa1a68e5b2bf46dd7fe822732260b8d8692f486f
Andy Edwards <jedwards@fastmail.com>
2025-08-20 16:25:25 -0500

    fix(@jcoreio/clarity-feature-api): ensure JSDoc is preserved in .d.ts files

--- packages/clarity-feature-api/src/client/ClientFeatureContributions.ts
+++ packages/clarity-feature-api/src/client/ClientFeatureContributions.ts
@@ -2,6 +2,32 @@ import z from 'zod'
 import * as React from 'react'
 import { CustomDashboardWidgetProps } from './CustomDashboardWidgetProps'
 
+// we declare this manually instead of using `z.input<typeof ClientFeatureContributions>` because
+// the JSDoc gets lost in .d.ts files
+/**
+ * Components and behaviors contributed to the client side of Clarity by a custom feature.
+ */
+export type ClientFeatureContributions = {
+  /**
+   * Custom dashboard widget types to add to Clarity.  The key is the unique
+   * `customVariant` stored in the dashboard config.
+   */
+  dashboardWidgets?: {
+    [customVariant in string]?: {
+      /**
+       * The display name shown in the drag source in the side bar for this widget type
+       */
+      displayName: string
+      /**
+       * The React.lazy()-wrapped component for rendering the content of the widget
+       */
+      component: React.LazyExoticComponent<
+        React.ComponentType<CustomDashboardWidgetProps>
+      >
+    }
+  }
+}
+
 const lazySymbol = React.lazy(() => null as any).$$typeof
 
 type LazyComponentSchema<Props = any> = z.ZodType<
@@ -18,37 +44,21 @@ const LazyComponentSchema = z.unknown().refine(
   'must be a React.lazy exotic component'
 )
 
-/**
- * Components and behaviors contributed to the client side of Clarity by a custom feature.
- */
 export const ClientFeatureContributions = z.strictObject({
-  /**
-   * Custom dashboard widget types to add to Clarity.  The key is the unique
-   * `customVariant` stored in the dashboard config.
-   */
   dashboardWidgets: z
     .record(
       z.string(),
       z
         .strictObject({
-          /**
-           * The display name shown in the drag source in the side bar for this widget type
-           */
           displayName: z.string(),
-          /**
-           * The React.lazy()-wrapped component for rendering the content of the widget
-           */
           component:
             LazyComponentSchema as LazyComponentSchema<CustomDashboardWidgetProps>,
         })
         .optional()
     )
     .optional(),
-})
-
-// use output type so that we get the JSDoc.  This would be a problem for userland code doing
-// `satisfies ClientFeatureContributions` if the input and output types differed, but input
-// types don't have JSDoc on them
-export type ClientFeatureContributions = z.output<
-  typeof ClientFeatureContributions
+}) satisfies z.ZodType<
+  ClientFeatureContributions,
+  any,
+  ClientFeatureContributions
 >
