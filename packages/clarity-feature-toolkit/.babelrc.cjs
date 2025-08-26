2b0b25c8978417c0307714b8585eb6ad535410c9
Andy Edwards <jedwards@fastmail.com>
2025-08-26 12:08:53 -0500

    chore: upgrade toolchains

--- packages/clarity-feature-toolkit/.babelrc.cjs
+++ packages/clarity-feature-toolkit/.babelrc.cjs
@@ -1,18 +1,5 @@
 /* eslint-env node, es2018 */
 module.exports = function (api) {
   const base = require('@jcoreio/toolchain-esnext/.babelrc.cjs')(api)
-  return {
-    ...base,
-    presets: base.presets.map((p) =>
-      Array.isArray(p) && p[0].endsWith('@babel/preset-env/lib/index.js') ?
-        [
-          p[0],
-          {
-            ...p[1],
-            exclude: [...(p[1].exclude || []), 'proposal-dynamic-import'],
-          },
-        ]
-      : p
-    ),
-  }
+  return { ...base }
 }
