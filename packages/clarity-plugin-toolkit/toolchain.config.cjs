eadef85ee9c5720bb137943b05d69c3f45c573dd
Andy Edwards <jedwards@fastmail.com>
2025-09-24 15:09:15 -0500

    chore: fix build again

--- packages/clarity-plugin-toolkit/toolchain.config.cjs
+++ packages/clarity-plugin-toolkit/toolchain.config.cjs
@@ -6,7 +6,7 @@ module.exports = {
   outputEsm: false, // disables ESM output (default: true)
   // esWrapper: true, // outputs ES module wrappers for CJS modules (default: false)
   scripts: {
-    pretest: 'tc build',
+    pretest: `tc build && (cd ../../fixtures/test-plugin && (pnpm remove @jcoreio/clarity-plugin-toolkit || true) && pnpm i '@jcoreio/clarity-plugin-toolkit@workspace:*')`,
     'build:smoke-test': {
       description: 'no-op',
       run: () => {},
