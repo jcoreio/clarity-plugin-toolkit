8b1b938ae7ff81967d41d7963e3b5c4039bf9bc0
Andy Edwards <jedwards@fastmail.com>
2025-10-03 18:38:02 -0500

    ci: work around tests that fail in coverage mode

--- packages/create-clarity-plugin/test/typescript.ts
+++ packages/create-clarity-plugin/test/typescript.ts
@@ -12,40 +12,43 @@ const fixtureDir = path.join(
   'test-create'
 )
 
-// tricky problems loading webpack.config.ts in CI, argh
-;(process.env.CI ? it.skip : it)('typescript', async function () {
-  this.timeout(60000)
-  await fs.remove(fixtureDir)
-  // eslint-disable-next-line no-console
-  console.log('fixture dir:', fixtureDir)
-  const options: TemplateOptions = {
-    name: 'testpkg',
-    clarityPluginToolkitDir: '.clarity-plugin-toolkit',
-    useToolchain: false,
-    toolchainVersion: '',
-    useTypescript: true,
-    useEslint: true,
-    usePrettier: true,
-    stubs: [
-      'dashboardWidget',
-      'expressApi',
-      'jsMigrations',
-      'organizationView',
-      'sidebarItem',
-      'sqlMigrations',
-    ],
-    packageManager: 'pnpm',
+// nyc interferes with TS-based webpack config loading
+;(process.env.JCOREIO_TOOLCHAIN_COVERAGE ? it.skip : it)(
+  'typescript',
+  async function () {
+    this.timeout(60000)
+    await fs.remove(fixtureDir)
+    // eslint-disable-next-line no-console
+    console.log('fixture dir:', fixtureDir)
+    const options: TemplateOptions = {
+      name: 'testpkg',
+      clarityPluginToolkitDir: '.clarity-plugin-toolkit',
+      useToolchain: false,
+      toolchainVersion: '',
+      useTypescript: true,
+      useEslint: true,
+      usePrettier: true,
+      stubs: [
+        'dashboardWidget',
+        'expressApi',
+        'jsMigrations',
+        'organizationView',
+        'sidebarItem',
+        'sqlMigrations',
+      ],
+      packageManager: 'pnpm',
+    }
+    const actual = await files(options)
+    for (const [file, content] of Object.entries(actual)) {
+      if (!content) continue
+      const destFile = path.join(fixtureDir, file)
+      await fs.mkdirs(path.dirname(destFile))
+      await fs.writeFile(destFile, content, 'utf8')
+    }
+    const execaOpts: execa.Options = { cwd: fixtureDir, stdio: 'inherit' }
+    await execa('pnpm', ['i'], execaOpts)
+    await execa('pnpm', ['run', 'format'], execaOpts)
+    await execa('pnpm', ['run', 'check'], execaOpts)
+    await execa('pnpm', ['run', 'build'], execaOpts)
   }
-  const actual = await files(options)
-  for (const [file, content] of Object.entries(actual)) {
-    if (!content) continue
-    const destFile = path.join(fixtureDir, file)
-    await fs.mkdirs(path.dirname(destFile))
-    await fs.writeFile(destFile, content, 'utf8')
-  }
-  const execaOpts: execa.Options = { cwd: fixtureDir, stdio: 'inherit' }
-  await execa('pnpm', ['i'], execaOpts)
-  await execa('pnpm', ['run', 'format'], execaOpts)
-  await execa('pnpm', ['run', 'check'], execaOpts)
-  await execa('pnpm', ['run', 'build'], execaOpts)
-})
+)
