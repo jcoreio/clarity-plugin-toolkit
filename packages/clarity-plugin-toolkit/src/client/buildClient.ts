3e66ccbf8612a33fee0e540883e69d714a8ad40c
Andy Edwards <jedwards@fastmail.com>
2025-10-03 11:07:12 -0500

    fix(@jcoreio/clarity-plugin-toolkit): exec webpack via project's package manager

--- packages/clarity-plugin-toolkit/src/client/buildClient.ts
+++ packages/clarity-plugin-toolkit/src/client/buildClient.ts
@@ -1,11 +1,16 @@
 import execa from 'execa'
 import getProject from '../getProject'
+import { getPackageManager } from '../util/getPackageManager'
 
 export async function buildClient(options?: { args?: string[] }) {
   const { projectDir, packageJson } = await getProject()
   if (!packageJson.clarity?.client) return
-  await execa('npm', ['exec', 'webpack', '--', ...(options?.args || [])], {
-    stdio: 'inherit',
-    cwd: projectDir,
-  })
+  await execa(
+    await getPackageManager(),
+    ['exec', 'webpack', '--', ...(options?.args || [])],
+    {
+      stdio: 'inherit',
+      cwd: projectDir,
+    }
+  )
 }
---
+++ packages/clarity-plugin-toolkit/src/util/getPackageManager.ts
@@ -0,0 +1,27 @@
+import getProject from '../getProject'
+
+export type PackageManager = 'npm' | 'pnpm' | 'yarn' | 'bun'
+
+export async function getPackageManager(): Promise<PackageManager> {
+  let userAgent = process.env.npm_config_user_agent
+  if (!userAgent) {
+    const {
+      packageJson: { packageManager },
+    } = await getProject()
+    userAgent = typeof packageManager === 'string' ? packageManager : ''
+  }
+
+  if (userAgent.startsWith('yarn')) {
+    return 'yarn'
+  }
+
+  if (userAgent.startsWith('pnpm')) {
+    return 'pnpm'
+  }
+
+  if (userAgent.startsWith('bun')) {
+    return 'bun'
+  }
+
+  return 'npm'
+}
