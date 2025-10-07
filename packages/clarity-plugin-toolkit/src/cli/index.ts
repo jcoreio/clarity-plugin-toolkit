ba52edc1d9d3a5c8e1645b9fa0989dd2acd5e306
Andy Edwards <jedwards@fastmail.com>
2025-10-07 12:43:20 -0500

    fix(@jcoreio/clarity-plugin-toolkit): show webpack build progress when terminal is interactive

--- packages/clarity-plugin-toolkit/src/cli/index.ts
+++ packages/clarity-plugin-toolkit/src/cli/index.ts
@@ -1,15 +1,45 @@
 #!/usr/bin/env node
 
+import chalk from 'chalk'
 import '../checkNodeVersion'
 import * as dotenv from 'dotenv'
+import path from 'path'
 
-dotenv.config({ quiet: true })
+const envValues: typeof process.env = {}
+dotenv.config({
+  quiet: true,
+  // @ts-expect-error dotenv types are in the wrong here
+  processEnv: envValues,
+})
+
+let warnedAboutInterpolation = false
+for (const key in envValues) {
+  if (key.startsWith('CLARITY_PLUGIN_TOOLKIT_')) {
+    const value = envValues[key]
+    if (value == null) continue
+    if (value.includes('$')) {
+      if (!warnedAboutInterpolation) {
+        // eslint-disable-next-line no-console
+        console.error(
+          chalk.yellow(
+            `Warning: clarity-plugin-toolkit doesn't support interpolation in .env yet (found ${key}=${value})`
+          )
+        )
+        warnedAboutInterpolation = true
+      }
+      continue
+    }
+    process.env[key] = value
+  }
+}
 
 import yargs from 'yargs/yargs'
 
 void yargs(process.argv.slice(2))
   .scriptName('clarity-plugin-toolkit')
-  .commandDir('commands')
+  .commandDir('commands', {
+    extensions: [path.extname(__filename).replace(/^\./, '')],
+  })
   .strict()
   .demandCommand()
   .help().argv
